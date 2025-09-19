d2c5461ac6418335b38bd1504ee10a88d86a4280
joaomap <joao_a_paes@ufms.br>
2025-09-18 21:17:15 -0400

    fix: arruma exercicios errados

--- ex_rec/ex3rec.cpp
+++ ex_rec/ex3rec.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #define MAX 101
 
-int inter(int va[], int vb[], int vc[], int c = 0){
-    vc[c] = va[c];
-    vc[c + 1] = vb[c];
-    if(va[c+1] != '\0') return inter(va,vb,vc, c+1);
-    return 0;
+void inter(int va[], int vb[], int vc[], int c, int a){
+    vc[2*c] = va[c];
+    vc[2*c + 1] = vb[c];
+    if(c < a) return inter(va,vb,vc, c+1, a);
+    return;
 }
+
 int main(){
     int a;
-    int va[MAX],vb[MAX],vc[MAX];
     scanf("%d", &a);
+    int va[a+1],vb[a+1],vc[MAX];
     for(int i = 0; i < a; i++){
         scanf("%d", &va[i]);
     }
     for(int i = 0; i < a; i++){
         scanf("%d", &vb[i]);
     }
-    inter(va, vb, vc);
-    for(int i : vc){
-        printf("%d ", i);
+    inter(va, vb, vc, 0, a);
+    for(int i = 0; i < 2*a; i++){
+        printf("%d ", vc[i]);
     }
 }
--- ex_rec/ex7rec.cpp
+++ ex_rec/ex7rec.cpp
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include <math.h>
 
-bool par(int n){
-    if(n % 2 != 0) return false;
-    return true;
+bool par (int n){
+	if(n % 2 == 0) return true;
+	return false;
 }
 
-int pot(int x, int n){
-    if(n == 0) return 1;
-    if(par(n)){
-        return x * 
-    }
-
+int potencia(int x, int n){
+	if (n == 0) return 1;
+	int p = par(n) ? potencia(x * x, n/2) : x * potencia(x, n - 1);
+	return p;
 }
+
 int main(){
-
+	int a,b;
+	scanf("%d %d", &a, &b);
+	printf("%d", potencia(a,b));
 }
